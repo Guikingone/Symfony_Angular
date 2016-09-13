c49d376ba7ca5e259ea09189caa6089a8a7b0ac0
Guikingone <guillaume.loulier@hotmail.fr>
2016-09-13 19:38:48 +0200

    [+][!M] || Angular Form|In Progress

--- web/dev/Angular/app.component.ts
+++ web/dev/Angular/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from "@angular/core";
 
+import { Article } from "./Component/Article/Article";
+
 @Component({
     selector: "my-app",
-    template: '<h1>{{ title }} par {{ author }}</h1>'
+    template: `
+        <label>Veuillez entrer les informations relatives a l'article : </label>
+            <input [(ngModel)]="article.title" placeholder="name">
+            <input [(ngModel)]="article.author" placeholder="Auteur" />
+            <input [(ngModel)]="article.tags" placeholder="Tags" />
+        <br />
+        <br />
+        <p>Voici votre recherche : <em>{{article.title}}</em> écrit par <strong>{{article.author}}</strong> dans les tags: {{ article.tags }}</p>
+        `
 })
 export class AppComponent {
-    title = 'Hello World';
-    author = 'Guikingone';
+    titre = 'Recherche !';
+    article: Article = {
+        title: '',
+        author: '',
+        date: Date(),
+        tags: []
+    };
 }
