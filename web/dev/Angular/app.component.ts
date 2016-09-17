58261dd62b8d409998a3fdaabf3d26f477eafa36
Guikingone <guillaume.loulier@hotmail.fr>
2016-09-17 20:00:10 +0200

    [+][!M] Angular + Bower|In progress

--- web/dev/Angular/app.component.ts
+++ web/dev/Angular/app.component.ts
@@ -1,23 +1,13 @@
 import { Component } from "@angular/core";
 
-import { Article } from "./Component/Article/Article";
-
 @Component({
     selector: "my-app",
     templateUrl: './dev/Angular/Templates/index.html.twig'
 })
 export class AppComponent {
-    titre = 'Recherche !';
-    article: Article = {
-        title: '',
-        author: '',
-        date: Date(),
-        tags: []
-    };
-    articles = [];
-    addArticles(newArticle: string) {
-        if (newArticle) {
-            this.articles.push(newArticle);
-        }
+    clickMessage = '';
+
+    onClickButton() {
+        this.clickMessage = "Vous venez de cliquer sur le bouton !";
     }
 }
